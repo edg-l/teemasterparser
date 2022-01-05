8ec50c7616848c444ba9feee3a18dc3936f703e4
Edgar Luque <git@edgarluque.com>
2022-01-05 15:52:29 +0100

    better2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 use serde::Deserialize;
 use std::io::{BufReader, Cursor, Read, Write};
 use tar::Archive;
-use time::{macros::date, Date, OffsetDateTime, Duration, ext::NumericalDuration};
+use time::{ext::NumericalDuration, macros::date, Date, Duration, OffsetDateTime};
 
 use clap::{App, Arg};
 use std::{fs, path::PathBuf};
@@ -68,7 +68,10 @@ fn main() -> anyhow::Result<()> {
     )
     .get_matches();*/
 
-    //rayon::ThreadPoolBuilder::new().num_threads(4).build_global().unwrap();
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(6)
+        .build_global()
+        .unwrap();
 
     let mut current_date = date!(2021 - 5 - 18);
     let yesterday = OffsetDateTime::now_utc().date().previous_day().unwrap();
@@ -80,7 +83,7 @@ fn main() -> anyhow::Result<()> {
         current_date = current_date.next_day().unwrap();
     }
 
-    dates.into_iter().par_bridge().for_each(|cur_date| {
+    dates.into_iter().rev().par_bridge().for_each(|cur_date| {
         create_plot(cur_date).expect("work");
     });
     Ok(())
@@ -88,7 +91,8 @@ fn main() -> anyhow::Result<()> {
 
 fn create_plot(cur_date: Date) -> anyhow::Result<()> {
     println!("Prcoessing {}", cur_date);
-    let path_regex: Regex = Regex::new(r#"(?P<hour>\d{2})_(?P<minute>\d{2})_(?P<second>\d{2}).json"#).unwrap();
+    let path_regex: Regex =
+        Regex::new(r#"(?P<hour>\d{2})_(?P<minute>\d{2})_(?P<second>\d{2}).json"#).unwrap();
 
     let resp = ureq::get(&format!(
         "https://ddnet.tw/stats/master/{}.tar.zstd",
@@ -109,27 +113,30 @@ fn create_plot(cur_date: Date) -> anyhow::Result<()> {
 
     let mut archive = Archive::new(decoder);
 
-    let plot_data = archive.entries()?.map(|entry| {
-        let entry = entry.unwrap();
-        let path = entry.path().unwrap();
-        let filename = path.file_name().expect("be a file");
-        let filename = filename.to_string_lossy();
+    let plot_data = archive
+        .entries()?
+        .step_by(12) // 60 seconds / every 5 seconds       
+        .map(|e| {
+            let entry = e.unwrap();
+            let path = entry.path().unwrap();
+            let filename = path.file_name().expect("be a file");
+            let filename = filename.to_string_lossy();
 
-        let captures = path_regex.captures(&filename).expect("match regex");
+            let captures = path_regex.captures(&filename).expect("match regex");
 
-        let hour: f64 = captures.name("hour").unwrap().as_str().parse().unwrap();
-        let minute: f64 = captures.name("minute").unwrap().as_str().parse().unwrap();
-        let second: f64 = captures.name("second").unwrap().as_str().parse().unwrap();
-
-        let data: ServerList = serde_json::from_reader(entry).expect("parse json");
-        let seconds = (hour * 60.0 * 60.0) + (minute * 60.0) + second;
-        let total_players = data
-            .servers
-            .into_iter()
-            .map(|x| x.info.clients.len())
-            .sum::<usize>() as f64;
-        (seconds, total_players)
-    }).collect_vec();
+            let hour: i64 = captures.name("hour").unwrap().as_str().parse().unwrap();
+            let minute: i64 = captures.name("minute").unwrap().as_str().parse().unwrap();
+            let second: i64 = captures.name("second").unwrap().as_str().parse().unwrap();
+            let seconds = (hour * 60 * 60) + (minute * 60) + second;
+            let data: ServerList = serde_json::from_reader(entry).expect("parse json");
+            let total_players = data
+                .servers
+                .into_iter()
+                .map(|x| x.info.clients.len())
+                .sum::<usize>() as f64;
+            (seconds as f64, total_players)
+        })
+        .collect_vec();
 
     let mut plotter = poloto::plot("Total players", "Time", "Count");
     plotter.line_fill("", &plot_data);
@@ -141,12 +148,13 @@ fn create_plot(cur_date: Date) -> anyhow::Result<()> {
         write!(fmt, "{:02}:{:02}:{:02}", hours, minutes, seconds)
     });
 
-    let mut file = fs::File::create(&format!("{}.svg", cur_date))?;
+    let mut file = fs::File::create(&format!("images/{}.svg", cur_date))?;
     write!(
         file,
         "{}",
         poloto::disp(|a| poloto::simple_theme(a, plotter))
     )
     .unwrap();
+    println!("wrote file");
     Ok(())
 }
