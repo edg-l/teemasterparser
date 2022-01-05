4a28bb5eb9d91eb10c11a4f5e74f955b3275e7bb
Edgar Luque <git@edgarluque.com>
2022-01-05 16:14:46 +0100

    generate md

---
+++ create_markdown.py
@@ -0,0 +1,10 @@
+from datetime import timedelta, date, datetime
+
+first_day = date(2021, 5, 18)
+now = datetime.now().date()
+
+print("# Player count statistics")
+
+for single_date in (first_day + timedelta(n) for n in range((now - first_day).days)):
+    print(f"## {single_date}")
+    print(f"![{single_date} data](images/{single_date}.svg)")
